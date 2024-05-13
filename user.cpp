b0544c3e923e31f8daf684ecd0e3ee7cdd2135c1
quexzz <kind654@ya.ru>
2024-05-13 20:10:52 +0400

    Добавлена функция проверка смерти игрока

--- user.cpp
+++ user.cpp
@@ -171,6 +171,14 @@ void MoveCameraTowards(Context &ctx, Object &obj, float dt)
 //
 bool CheckPlayerDeath(Object &player, Scene &scene)
 {
+    for (auto& obj : scene) {
+        if (obj.enemy.enabled) {
+            Collision collision = CheckCollision(player, obj);
+            if (collision.exists) {
+                return true;
+            }
+        }
+    }
     return false;
 }
 
